99da98781c7bff96fc9e1d02937a3a550d17dad7
Wanderson Soares <wandersonsoares841@gmail.com>
2024-05-14 08:57:32 -0300

    add parser

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 #include "inc/sh.h"
 #include "inc/parser.h"
+#include "inc/exec.h"
 
 
 int main () {
@@ -13,17 +14,21 @@ int main () {
     cshell_init();
 
     char input[MAX_CHAR_INPUT];
-    char **params = NULL;
     char *prompt;
     int cpid;
+
+    Parsed_input p_input;
+    parser_init(&p_input);
     
     while (1) {
         prompt = getenv("PS1");
         fprintf(stdout, prompt);
-        fscanf(stdin, "%s", input);
+        fgets(input, MAX_CHAR_INPUT, stdin);
 
         if (feof(stdin))    break;
-        
+
+        process_input(input, &p_input);
+
         pid_t pid = fork();
         
         if (pid < 0) {
@@ -34,10 +39,12 @@ int main () {
             cpid = waitpid(pid, NULL, 0);
         }
         else {
-            execve(input, params, NULL);
+            exec(&p_input);
             if (errno)  perror("shell");
        }
     }
+
+    parser_free(&p_input);
 
     return EXIT_SUCCESS;
 }
