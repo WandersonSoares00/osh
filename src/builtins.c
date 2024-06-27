e1ea8f9c87b4cb6997c60eaa0d91fcf22fbb051c
Wanderson Soares <wandersonsoares841@gmail.com>
2024-06-27 11:50:03 -0300

    add setenv

--- src/builtins.c
+++ src/builtins.c
@@ -26,15 +26,27 @@ int cshell_exit(char **argv) {
     exit(EXIT_FAILURE);
 }
 
+int set_env(char **argv) {
+    if (!argv[1] || !argv[2]) {
+        fprintf(stderr, "setenv: usage: setenv [name] [value]\n");
+        return 1;
+    }
+
+    setenv(argv[1], argv[2], 1);
+    return 0;
+}
+
 char *builtin_str[] = {
   "cd",
   "exit",
+  "setenv",
   NULL
 };
 
 int (*builtin_func[]) (char **) = {
   &cshell_cd,
   &cshell_exit,
+  &set_env,
   NULL
 };
 
